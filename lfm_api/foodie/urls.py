13fd33bfec41046967d223a5c876c7bfe4843fea
sparkh <sparkh42@gmail.com>
2016-03-26 18:14:36 +0100

    add foodie urls file for CRUD

---
+++ lfm_api/foodie/urls.py
@@ -0,0 +1,21 @@
+# Django imports
+from django.conf.urls import url
+
+# View imports
+from .views.foodie import FoodieViews
+
+
+#
+# URLS
+#
+
+urlpatterns = [
+    # url(r'^auth/login/$'),
+    # url(r'^auth/logout/$'),
+    url(r'^auth/register/$', FoodieViews.as_view({'post': 'create'}), name='register'),
+
+    #url(r'^profile/$'),
+
+    url(r'^$', FoodieViews.as_view({'get': 'list'}), name='foodies'),
+    url(r'^(?P<pk>[0-9]+)/$', FoodieViews.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'delete'}), name='info'),
+]
