28bfeab915b0e669d3a86e8927caa0d4a86dd0d9
sparkh <sparkh42@gmail.com>
2016-04-06 10:15:20 +0200

    Create a BaseUser with all foodie and restaurant common fields

--- lfm_api/lfm_api/settings.py
+++ lfm_api/lfm_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user',
     'foodie',
     'restaurant',
 ]
@@ -87,7 +88,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
