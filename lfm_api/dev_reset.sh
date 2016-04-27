c6b4573915358d5f83cdbc906e03b8397f479c0c
sparkh <sparkh42@gmail.com>
2016-04-27 10:17:24 +0200

    Create a temporary file for simplify the reset of the local DB

---
+++ lfm_api/dev_reset.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+#
+#
+#
+
+remove_migration() {
+    rm -f ./*/migrations/*_initial.py
+    echo 'Migration files are now removed.'
+}
+
+remove_db() {
+    rm -f *.sqlite3
+    echo 'DB files are now removed.'
+}
+
+
+#
+#
+#
+
+remove_migration
+
+remove_db
