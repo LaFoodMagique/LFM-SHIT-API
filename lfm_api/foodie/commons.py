5e010d6159303b53635e2b3106ca9ce73acc486d
sparkh <sparkh42@gmail.com>
2016-05-04 08:44:43 +0200

    Add MARKS "enum" for the notation of the comments (dish and restaurant)

--- lfm_api/foodie/commons.py
+++ lfm_api/foodie/commons.py
@@ -13,3 +13,16 @@ def part_of(obj, list_obj):
         if o is obj:
             return True
     return False
+
+
+#
+# Marks
+#
+
+MARKS = (
+    (0, 'Very bad'),
+    (1, 'Bad'),
+    (2, 'Normal'),
+    (3, 'Good'),
+    (4, 'Very good'),
+)
